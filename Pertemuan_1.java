1df9d2c3630e18c35dbc871bdddab3015f286afd
rakaalpiansyah <144642330+rakaalpiansyah@users.noreply.github.com>
2023-09-13 17:11:37 +0700

    Toko lava

---
+++ Pertemuan_1.java
@@ -0,0 +1,17 @@
+class Main1 {
+    public static void main(String[] args) {
+        String RAKA = "-_--_--_--_--_--_--_--_--_--_--_--_--_--_-";
+        RAKA += "\n Selamat Datang di TokoLava";
+        RAKA += "\n Toko Terbaik di Tahun Depan";
+        RAKA += "\n Apa Yang Anda Butuhkan Pasti Tidak Tersedia";
+        RAKA += "\n -_--_--_--_--_--_--_--_--_--_--_--_--_--_-";
+        RAKA += "\n1 Kopi Gajah 5K";
+        RAKA += "\n2 Teh Tarik 10K";
+        RAKA += "\n3 Es Jeruk 7K";
+        RAKA += "\n4 Thao Tea 10K";       
+        RAKA += "\n5 Gorengan 1K";
+        RAKA += "\n6 Aoka 3K";
+        RAKA += "\n -_--_--_--_--_--_--_--_--_--_--_--_--_--_-";
+        System.out.println(RAKA);
+    }
+}
